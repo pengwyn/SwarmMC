923b8bf8191584e77cde6b9f44a626cab75e412a
Daniel Cocks <daniel.cocks@gmail.com>
2020-09-11 14:29:37 +1000

    Deploy docs

--- test/test_Argon.jl
+++ test/test_Argon.jl
@@ -2,6 +2,6 @@ import Argon
 
 cd(joinpath(dirname(pathof(SwarmMC)), "..", "examples","SwarmMCBenchmarks")) do
     params = Argon.SetupParams()
-    params.t_grid = LinRange(0, params.t_grid[end]/100, 10)
+    params.t_grid = LinRange(0*SwarmMC.uT, params.t_grid[end]/100, 10)
     props = BunchedPropagate(params,2)
 end
