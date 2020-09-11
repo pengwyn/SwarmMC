a9bc9aae7561569194b6a21d9c8f5808140eedab
Daniel Cocks <daniel.cocks@gmail.com>
2020-09-11 14:42:34 +1000

    Error in test fixed

--- test/test_ReidAniso.jl
+++ test/test_ReidAniso.jl
@@ -4,7 +4,7 @@ using NonNegLeastSquares
 @testset "Model $model, method $method" for model in [nothing ; collect("ABCD")],
     method in [:func,:grid,:legendre]
 
-    params = Finalise(ReidAniso.SetupParams(model, method))
+    params = ReidAniso.SetupParams(model, method)
     params.t_grid = LinRange(0*SwarmMC.uT, params.t_grid[end]/100, 101)
     props = BunchedPropagate(params,2)
 end
